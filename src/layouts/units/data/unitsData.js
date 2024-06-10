5c81f813af8aa80c56e85c280bd1b573c7c09982
bernaaaaak <bernaaaak@gmail.com>
2024-06-10 21:11:00 +0300

    unit start devextreme

--- src/layouts/units/data/unitsData.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function unitsData() {
-  return <div>unitsData</div>;
-}
-
-export default unitsData;
--- src/layouts/units/index.js
+++ src/layouts/units/index.js
@@ -1,9 +1,20 @@
 import React, { useEffect, useState } from "react";
 import "./style.css";
 import axios from "axios";
-import { DataGrid, Editing, Column, ValidationRule, Button } from "devextreme-react/data-grid";
+//import { DataGrid, Editing, Column, ValidationRule, Button } from "devextreme-react/data-grid";
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import DashboardNavbar from "examples/Navbars/DashboardNavbar";
+import DataGrid, {
+  Column,
+  FilterRow,
+  HeaderFilter,
+  FilterPanel,
+  Scrolling,
+  Editing,
+  ValidationRule,
+  Button,
+} from "devextreme-react/data-grid";
+import CustomStore from "devextreme/data/custom_store";
 
 const allowDeleting = (e) => e.row.data.ID !== 1;
 
@@ -23,7 +34,7 @@ const Units = () => {
 
   const fetchEmployees = async () => {
     try {
-      const response = await axios.get("/api/employees");
+      const response = await axios.get("/StockCardUnit/Filter");
       setEmployees(response.data);
     } catch (error) {
       console.error("Error fetching employees:", error);
@@ -32,7 +43,7 @@ const Units = () => {
 
   const handleAddEmployee = async (newData) => {
     try {
-      const response = await axios.post("/api/employees", newData);
+      const response = await axios.post("/StockCardUnit/Filter", newData);
       fetchEmployees();
     } catch (error) {
       console.error("Error adding employee:", error);
@@ -41,7 +52,7 @@ const Units = () => {
 
   const handleUpdateEmployee = async (key, updatedData) => {
     try {
-      await axios.put(`/api/employees/${key}`, updatedData);
+      await axios.put(`/StockCardUnit/Filter/${key}`, updatedData);
       fetchEmployees();
     } catch (error) {
       console.error("Error updating employee:", error);
@@ -50,12 +61,123 @@ const Units = () => {
 
   const handleDeleteEmployee = async (key) => {
     try {
-      await axios.delete(`/api/employees/${key}`);
+      await axios.delete(`/StockCardUnit/Filter/${key}`);
       fetchEmployees();
     } catch (error) {
       console.error("Error deleting employee:", error);
     }
   };
+
+  const dataSource = new CustomStore({
+    load: async (loadOptions) => {
+      try {
+        console.warn(loadOptions);
+        const response = await axios.post("/StockCardUnit/Filter", loadOptions);
+        return {
+          data: res.Items || [],
+          totalCount: res.TotalCount || 0,
+        };
+      } catch (error) {
+        console.error("Error adding employee:", error);
+      }
+    },
+  });
+
+  //   this.mainDataSource = new CustomStore({
+  //     key: 'FootPrintId',
+  //     load: (loadOptions: any) => {
+
+  //       that.mainDatagrid.instance.clearSelection();
+  //       if (this.dateRange.length < 2) {
+  //         return Promise.reject("Başlangıç ve bitiş tarihleri girmelisiniz!");
+  //       }
+
+  //       let filterFormData: any = {
+  //         beginDate: this.dateRange[0],
+  //         endDate: this.dateRange[1],
+  //       }
+
+  //       let body = that.prepareFilterBody(loadOptions, filterFormData);
+  //       return that.httpList(api/footprints, body);
+  //     },
+  //     }); //TODO : İNSERT UPDATE VS ALSO
+
+  // this.mainDataSource = new CustomStore({
+  //   key: 'FraudTypesId',
+  //   load: (loadOptions: any) => {
+
+  //     loadOptions.key = 'Name';
+  //     let body = that.prepareFilterBody(loadOptions, null);
+
+  //     return that.httpList(api/fraud-types, body);
+  //   },
+  //   insert: (values) => {
+
+  //     return that.httpPost(api/fraud-type/insertOrUpdate, values)
+  //       .then((res: any) => {
+
+  //         if (!res.HasError)
+  //           notify("Yeni kayıt işlemi tamamlandı", "success", 1500);
+  //       })
+  //       .catch(error => {
+  //         console.log("api/fraud-type/insertOrUpdate : " + error);
+  //         throw "Hatalı işlem!"
+  //       });
+  //   },
+  //   update: (key, values) => {
+
+  //     let post_values = { ...that.mainDatagrid.instance.getDataSource().items().find(i => i.FraudTypesId == key), ...values };
+  //     return that.httpPost(api/fraud-type/insertOrUpdate, post_values)
+  //       .then((res: any) => {
+
+  //         if (!res.HasError)
+  //           notify("Güncelleme işlemi tamamlandı", "success", 1500);
+  //       })
+  //       .catch(error => {
+  //         console.log("api/fraud-type/insertOrUpdate : " + error);
+  //         throw "Hatalı işlem!"
+  //       });
+  //   },
+  //   remove: (key) => {
+
+  //     return that.httpDelete(api/fraud-type/${key}/delete)
+  //       .then((res: any) => {
+
+  //         if (!res.HasError)
+  //           notify("Silme işlemi tamamlandı", "success", 1500);
+  //       })
+  //       .catch(error => {
+  //         console.log(`api/fraud-type/${key}/delete : ` + error);
+  //         throw "Hatalı işlem!"
+  //       });
+  //       }
+  //     });
+
+  // httpList(url: string, body: any) {
+
+  //   let that = this;
+
+  //   if(this.hasGlobalLoading)
+  //     this._loadPanelService.showLoader();
+
+  //   return lastValueFrom(this.http.post<any>(${this._appConfigService.inapiUrl}/${url}, body))
+  //     .then((res: any) => {
+
+  //       if (res.Success || res.success)
+  //        return Promise.resolve({
+  //           data: res.Items || res.items || [],
+  //           totalCount: res.TotalCount || res.totalCount || 0,
+  //         });
+  //       else
+  //         Promise.reject(res.Error || res.error || "Sunucu hatası!")
+  //     })
+  //     .finally(() => {
+
+  //       if(this.hasGlobalLoading)
+  //         this._loadPanelService.hideLoader();
+  //
+  //       });
+  //   }
 
   useEffect(() => {
     fetchEmployees();
@@ -85,6 +207,12 @@ const Units = () => {
             allowAdding={true}
             mode="form"
           />
+
+          <FilterRow visible={true} />
+          <FilterPanel visible={true} />
+          <HeaderFilter visible={true} />
+          <Scrolling mode="infinite" />
+
           <Column dataField="Prefix" caption="ProductGroupId">
             <ValidationRule type="required" />
           </Column>
